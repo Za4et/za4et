61e66f951828756f416992718b70b2a4273ab32c
zorkgb05@gmail.com <qazwsxedc1q>
2018-05-22 21:20:44 +0300

    last commit(probably)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from django.contrib.auth.views import logout
 from django.urls import path
+
 from main.decorators import check_recaptcha
 from . import views
 
@@ -12,7 +13,7 @@ urlpatterns = [
     url(r'news/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.article, name='article'),
     path('account/', views.account, name='account'),
     path('login/', check_recaptcha(views.logn), name='login'),
-    path('logout/', logout),
+    path('logout/', logout, {'template_name': 'zach/home.html'}),
     path('journals/', views.journals, name='journals'),
     path('library/', views.library, name='library'),
     path('questions/', views.questions, name='questions'),
